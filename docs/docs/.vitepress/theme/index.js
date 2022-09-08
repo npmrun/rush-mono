119b31c82aee77c672e55b239557d64be26a92ac
npmrun <kk1549469775@gmail.com>
2022-09-09 01:20:36 +0800

    fixed bug

--- docs/docs/.vitepress/theme/index.js
+++ docs/docs/.vitepress/theme/index.js
@@ -1,10 +1,12 @@
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
 /**
- * 注意这里因为是软链接，因此需要在feather-ui的package.json中添加以下依赖使其能够找到依赖位置
+ * 
+1.注意这里因为是软链接，因此需要在feather-ui的package.json中添加以下依赖使其能够找到依赖位置
 "devDependencies": {
     "feather-ui": "."
 }
+2.或者在根目录添加依赖也行，采用这种方法
  */
 import FUI from 'feather-ui/dist/components/dialog'
 import CUI from 'cloud-ui/dist/components/dialog'
