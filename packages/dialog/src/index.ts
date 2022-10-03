f639da9f274a9f7e784f732648d23637969e90b3
npmrun <1549469775@qq.com>
2022-10-03 11:54:24 +0800

    fixed bug

--- packages/dialog/src/index.ts
+++ packages/dialog/src/index.ts
@@ -1,9 +1,9 @@
 import RushDialog from './index.vue'
 import { App } from 'vue'
 
-RushDialog.name = 'rush-mask'
+RushDialog.name = 'rush-dialog'
 RushDialog.install = function (app: App, options: any) {
-    app.component('rush-mask', RushDialog)
+    app.component('rush-dialog', RushDialog)
 }
 export { RushDialog }
 export default RushDialog
