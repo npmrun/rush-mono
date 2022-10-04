4bfd5f0f12db1c90a6a5addc9c84c32a38cb1232
npmrun <1549469775@qq.com>
2022-10-04 14:17:03 +0800

    fixed bug

--- packages/dialog/src/index.vue
+++ packages/dialog/src/index.vue
@@ -67,11 +67,14 @@ function close() {
     position: absolute;
     inset: 0;
     z-index: 999;
+    display: flex;
+
     .niu-dialog__content {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
+        margin: 0;
+        // position: absolute;
+        // left: 50%;
+        // top: 50%;
+        // transform: translate(-50%, -50%);
     }
 }
 
