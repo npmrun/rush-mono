c995ccf6b24ed7c870ee3f75b0d488ba570f1f21
npmrun <1549469775@qq.com>
2022-11-03 00:14:35 +0800

    fixed bug

--- packages/feather/src/components/input/input.vue
+++ packages/feather/src/components/input/input.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="fe-input">
         <div class="fe-input__warpper">
-            <input class="fe-input__inner" value="231"/>
+            <input @focus="handleFocus" @blur="handleBlur" spellcheck="false" class="fe-input__inner" value="sasa"/>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+import { computed, ref } from 'vue';
+
+const isFocus = ref(false)
+function handleFocus(e: FocusEvent) {
+    isFocus.value = true
+}
+function handleBlur(e: Event) {
+    isFocus.value = false
+}
+
+const computedClass = computed(()=>{
+    return {
+        "":""
+    }
+})
 
 </script>
 
